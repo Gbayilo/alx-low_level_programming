49ec1c975ed0bdf30079d376b06feb3187d506c4
Gbayilo <princegeeby1@gmail.com>
2022-10-19 09:21:34 +0000

    my commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,16 +7,20 @@ int main(void)
 {
 	int x, y;
 
-	for (x = 0; x < 9; x++)
+	for (x = 48; x <= 56; x++)
 	{
-		for (y = x + 1; y < 10; y++)
+		for (y = 49; y <= 57; y++)
 		{
-			putchar((x % 10) + '0');
-			putchar((y % 10) + '0');
-			if (x == 8 && y  == 9)
-				continue;
-			putchar(',');
-			putchar(' ');
+			if (y > x)
+			{
+				putchar(x);
+				putchar(y);
+				if (x != 56 || y != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
 	}
 	putchar('\n');
