64ebadb8e8ca068cec86b93795142551d0165293
Gbayilo <princegeeby1@gmail.com>
2022-10-19 10:04:04 +0000

    my commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,9 +8,9 @@ int main(void)
 	int n, m, l;
 
 	for (n = 48; n < 58; n++)
-	{	
+	{
 		for (m = 49; m < 58; m++)
-		{	
+		{
 			for  (l = 50; l < 58; l++)
 			{
 				if (l > m && m > n)
@@ -22,10 +22,8 @@ int main(void)
 					{
 						putchar(',');
 						putchar(' ');
-				        }
+					}
 				}
-
-				
 			}
 		}
 	}
