6a078188ea4e4a605554684e4b28df3f3bd8eb4b
oluwagbayilo <princegeeby1@gmail.com>
2023-08-14 19:08:19 +0100

    recreate file
    file contain c program that prints certain text

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
-/**
- * main - entry point
- * Return: Always 0
- */
-int main(void)
+
+int main()
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("Programming is like building a multilingual puzzle");
+	return 0;
 }
