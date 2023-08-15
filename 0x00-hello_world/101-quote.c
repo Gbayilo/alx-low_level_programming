55915aa2d7871cf07a99c8e85283a8b1b274781e
oluwagbayilo <princegeeby1@gmail.com>
2023-08-15 18:01:58 +0100

    update code for file 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,10 @@
-#include <unistd.h>
+#include <stdio.h>
 /**
  * main - entry point
  * Return: Always 1
  */
 int main(void)
 {
-	write(2,
-		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-		59);
+	fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
