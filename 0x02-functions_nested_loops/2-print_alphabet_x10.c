21d60d3911096a0a215b5012045f72cf53535a19
oluwagbayilo <princegeeby1@gmail.com>
2023-08-16 14:41:50 +0100

    Update task 2 to betty coding style

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,24 +1,28 @@
 #include "main.h"
 
 /**
- * print print_alphabet_x10 -print 10 times the alphabet, in lowercase,
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
  * followed by a new line.
  * Return 0
  */
 
 void print_alphabet_x10(void)
-{	
+{
 	char character;
+
 	int  i = 0;
+
 	while (i < 10)
 	{
 		character = 'a';
+
 		while (character <= 'z')
 		{
 			_putchar(character);
 			character++;
 		}
 		_putchar('\n');
+
 		i++;
 	}
 }
