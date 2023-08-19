3389d650c4dd9f647a8448d46d80ae6dd2558efe
oluwagbayilo <princegeeby1@gmail.com>
2023-08-19 06:13:29 +0100

    Adds solution to task 11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - entry point for cde
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+	long number = 612852475143;
+
+	for (a = (int) sqrt(number); a > 2; a++)
+	{
+		if (number % c == 0)
+		{
+			print("%i\n", a);
+			break;
+		}
+	}
+
+	return (0);
+}
