8a0f2dab973e3a1903402930b7a98660b5b2c2ca
oluwagbayilo <princegeeby1@gmail.com>
2023-08-19 05:22:00 +0100

    Fix errors in code

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,17 +8,26 @@
 
 void print_diagonal(int n)
 {
-	int j, k;
-
 	if (n <= 0)
-		_putchar('\n');
-
-	for (j = 0;  j < n; j++)
 	{
-		for (k = 0; k < j; k++)
-			_putchar(' ');
-	}
-		_putchar('\\');
 		_putchar('\n');
+	}
+	else
+	{
+		int j, k;
+
+		for (j = 0; j < n; j++)
+		{
+			for (k = 0; k < n; k++)
+			{
+				if (k == j)
+					_putchar('\\');
+				else if (k < j)
+					_putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
+
 
 }
