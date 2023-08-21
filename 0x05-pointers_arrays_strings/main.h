51ddd45ba76b582a02662380a88e01f0bc23e2af
oluwagbayilo <princegeeby1@gmail.com>
2023-08-21 18:14:47 +0100

    Fix error with main.h file

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -3,8 +3,8 @@
 
 int _putchar(char c);
 void reset_to_98(int *n);
-void swap_int(int *a, int *b)i
-int _strlen(char *s);i
+void swap_int(int *a, int *b);
+int _strlen(char *s);
 int _strlen(char *s);
 
 
