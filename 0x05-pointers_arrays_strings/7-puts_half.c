c73764d8965d084728b71b55dcbbebc2a1a92e2d
oluwagbayilo <princegeeby1@gmail.com>
2023-08-21 20:58:06 +0100

    Creates file 7-puts_half.c
    File adds solutin to task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints half of a string, followed by a new line.
+ * @str: pointer to the string to be printed
+ *
+ */
+void puts_half(char *str)
+{
+	int i, length, index;
+
+	length = 0;
+
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	if (length % 2 == 0)
+	{
+		index = length / 2;
+	}
+	else
+	{
+		index = (length - 1) / 2;
+	}
+
+	for (i = index; i < length; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
+
