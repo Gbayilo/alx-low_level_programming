8ba17cf3980c8500c2658968cc4b918e0f137e30
oluwagbayilo <princegeeby1@gmail.com>
2023-08-21 19:42:03 +0100

    Creates file 5-rev_string.c
    File adds solution to task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string
+ * @s: Pointer to the string to be reversed
+ *
+ */
+void rev_string(char *s)
+{
+	int lengthofString, i;
+
+	while (s[lengthofString] != '\0')
+	{
+		lengthofString++;
+	}
+	for (i = 0; i < lengthofString / 2; i++)
+	{
+		char temp = s[i];
+
+		s[i] = s[lengthofString - 1 - i];
+		s[lengthofString - 1 - i] = temp;
+	}
+}
