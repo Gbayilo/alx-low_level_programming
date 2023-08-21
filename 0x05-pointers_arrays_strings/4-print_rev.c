1b30c542379e53f70bbe1c64a2de36c99dfd4010
oluwagbayilo <princegeeby1@gmail.com>
2023-08-21 18:54:27 +0100

    Creates file 4-print_rev.c
    File adds solution to task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_rev - Prints a string, in reverse,
+ * followed by a new line.
+ * @s: Pointer to the string to be printed in reverse
+ *
+ */
+void print_rev(char *s)
+{
+	int len;
+
+	len = 0;
+
+	for (; s[len] != '\0'; len++)
+	{
+		int i;
+
+		for (i = len - 1; i >= 0;  i--)
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
