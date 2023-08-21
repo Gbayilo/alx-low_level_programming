a99609e93c394f6d78c9f5f2881eef1cc5d41abb
oluwagbayilo <princegeeby1@gmail.com>
2023-08-21 19:50:31 +0100

    Fix error in code

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,6 +8,7 @@
 void rev_string(char *s)
 {
 	int lengthofString, i;
+	char temp;
 
 	while (s[lengthofString] != '\0')
 	{
@@ -15,7 +16,7 @@ void rev_string(char *s)
 	}
 	for (i = 0; i < lengthofString / 2; i++)
 	{
-		char temp = s[i];
+		temp = s[i];
 
 		s[i] = s[lengthofString - 1 - i];
 		s[lengthofString - 1 - i] = temp;
