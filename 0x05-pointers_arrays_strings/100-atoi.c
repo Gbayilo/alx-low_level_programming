9966a59c559f394473cc59f136a7fe8bbaa6d165
oluwagbayilo <princegeeby1@gmail.com>
2023-08-22 02:45:35 +0100

    Creates file 100-atoi.c
    file contains solution to task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,1 +1,25 @@
+#include "main.h"
+
+/**
+ * _atoi - Convert string to an integer.
+ * @s: pointer to the string to be converted.
+ *
+ * Return: The converted integer.
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int number = 0;
+
+	do {
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && s <= '9')
+			number = (number * 10) + (*s - '0');
+		else if (number > 0)
+			break;
+	} while (*s++);
+
+	return (number * sign);
+}
 
