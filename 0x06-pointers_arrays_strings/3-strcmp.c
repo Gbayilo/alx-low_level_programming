8484c9588358d7b2321781c4433244d50b6a4bf6
oluwagbayilo <princegeeby1@gmail.com>
2023-08-24 19:40:32 +0100

    Fix code

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,19 +11,19 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-
-	while (i = 0)
-
+	while (((*s1 != '\0') && (*s2 != '\0')) && (*s1 == *s2))
 	{
-		s1[i] != '\0' || s2[i] != '\0';
-		i++;
-
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+		s1++;
+		s2++;
 	}
-
-	return (0);
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+	else
+	{
+		return (*s1 - *s2);
+	}
 }
 
 
