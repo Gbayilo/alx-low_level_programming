c48b4c74f4af2ff6fc63d6d678f46a7574e1c5f1
oluwagbayilo <princegeeby1@gmail.com>
2023-08-25 17:18:12 +0100

    Fix errors in code

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,20 +6,23 @@
  */
 void print_number(int n)
 {
+	unsigned int num;
+
 	if (n < 0)
 	{
+		num = -n;
 		_putchar('-');
-		n = -n;
+
 	}
 	else
 	{
-		n = n;
+		num = n;
 	}
-	if (n / 10 != 0)
+	if (num / 10)
 	{
-		print_number(n / 10);
+		print_number(num / 10);
 
 	}
 
-	_putchar(n % 10 + '0');
+	_putchar(num % 10 + '0');
 }
