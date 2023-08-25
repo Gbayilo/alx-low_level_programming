7572d3832c071eba3802ad50f59adf0677c83b36
oluwagbayilo <princegeeby1@gmail.com>
2023-08-25 13:27:55 +0100

    Fix errors in code

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,23 +9,26 @@
 char *cap_string(char *str)
 {
 	int i, j;
+	char a[] = " \t\n,;.!?\"(){}";
 
 	i = 0;
 
-	char a[] = " \t\n,;.!?\"(){}";
 
-	while (*(str + i))
+	while (str[i])
 	{
-		if (*(str + i) >= 'a' && *(str + i) <= 'z')
+		if (str[i] >= 'a' && str[i] <= 'z')
 		{
 			if (i == 0)
-				*(str + i) -= 'a' - 'A';
+				str[i] -= 'a' - 'A';
 			else
 			{
-				for (j = 0; j <= 12; j++)
+				for (j = 0; a[j] != '\0'; j++)
 				{
-					if (a[j] == *(str + i - 1))
-						*(str + i) -= 'a' - 'A';
+					if (a[j] == str[i - 1])
+					{
+						str[i] -= 'a' - 'A';
+						break;
+					}
 				}
 			}
 
