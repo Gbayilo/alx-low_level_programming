27360c83bc17bc0b9eea253ba12bdb5db8ab6727
oluwagbayilo <princegeeby1@gmail.com>
2023-08-25 13:00:12 +0100

    Adds solution to task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - Encodes a string into "1337"
+ * @str: The input string.
+ *
+ * Return: A pointer to modified string.
+ */
+
+char *leet(char *str)
+{
+	int i, j;
+
+	char *initial =  str;
+
+	for (int i = 0; str[i] != '\0'; i++)
+	{
+		for (int j = 0; "aeotlAEOTL"[j] != '\0'; j++)
+		{
+			if (str[i] == "aeotlAEOTL"[j])
+			{
+				str[i] = "43071043"[j];
+				break;
+			}
+		}
+	}
+	return (initial);
+}
