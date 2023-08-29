89c1190708b86bbe5f9a8cf48fb0a4864558ea1c
oluwagbayilo <princegeeby1@gmail.com>
2023-08-29 17:52:39 +0100

    Adds solution to task 16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ *
+ * Description: prints the first 98 Fibonacci numbers, starting with 1 and 2,
+ * followed by a new line.
+ *
+ * Return: Always 0 on success.
+ */
+int main(void)
+{
+	unsigned long a1, a2, a3, sum1;
+	unsigned long b1, b2, b3, sum2;
+	int i;
+
+	printf("1, 2");
+
+	for (a1 = 1, b1 = 2, i = 1; i < 90; i++)
+	{
+		sum1 = a1 + b1;
+		printf(", %ld", sum1);
+		a1 = b1;
+		b1 = sum1;
+	}
+
+	printf(", ");
+	a2 = a1 % 1000;
+	a1 = a1 / 1000;
+	b2 = b1 % 1000;
+
+	for (; i < 97; i++)
+	{
+		sum2 = (a2 + b2) / 1000;
+		a3 = (a2 + b2) - sum2 * 1000;
+		b3 = (a1 + b1) + sum2;
+		a2 = b2;
+		b2 = a3;
+
+		if (a3 >= 100)
+			printf("%lu%lu", b3, a3);
+		else
+			printf("%lu0%lu", b3, a3);
+		if (i != 96)
+			printf(", ");
+	}
+	printf("\n");
+	return (0);
+
+}
+
