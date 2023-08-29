fde9ec8e2b17f037caca5c4976ed7eb79fc4dc56
oluwagbayilo <princegeeby1@gmail.com>
2023-08-29 22:40:01 +0100

    Adjusts solution to task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -15,20 +15,17 @@ int main(void)
 	int i;
 
 	printf("1, 2");
-
 	for (a1 = 1, b1 = 2, i = 1; i < 90; i++)
 	{
 		sum1 = a1 + b1;
-		printf(", %ld", sum1);
+		printf(", %lu", sum1);
 		a1 = b1;
 		b1 = sum1;
 	}
-
 	printf(", ");
 	a2 = a1 % 1000;
 	a1 = a1 / 1000;
 	b2 = b1 % 1000;
-
 	for (; i < 97; i++)
 	{
 		sum2 = (a2 + b2) / 1000;
@@ -36,16 +33,20 @@ int main(void)
 		b3 = (a1 + b1) + sum2;
 		a2 = b2;
 		b2 = a3;
+		if (b3 > 0)
+		{
+			if (a3 >= 100)
+				printf("%lu%03lu", b3, a3);
+			else if (a3 >= 10)
+				printf("%lu%02lu", b3, a3);
+			else
+				printf("%lu", a3);
 
-		if (a3 >= 100)
-			printf("%lu%lu", b3, a3);
-		else
-			printf("%lu0%lu", b3, a3);
 		if (i != 96)
 			printf(", ");
+		}
 	}
 	printf("\n");
 	return (0);
-
 }
 
