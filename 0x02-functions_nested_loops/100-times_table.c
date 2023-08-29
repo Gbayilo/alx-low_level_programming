ed5aed1be431ec40cee839395148ba28c364d7ff
oluwagbayilo <princegeeby1@gmail.com>
2023-08-29 16:40:34 +0100

    Adjusts solution to task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,30 +6,44 @@
  */
 void print_times_table(int n)
 {
-	int i, j;
+	int i, j k;
 
-	if (n < 0 || n > 15)
-		return;
-	for (i = 0; i <= n; i++)
+	if (n >= 0 && n <= 15)
 	{
-		for (j = 0; j <= n; j++)
+		for (i = 0; i < n + 1; i++)
 		{
-			int result = i * j;
-
-			if (j > 0)
-				_putchar(',');
-
-			_putchar(' ');
-
-			if (result < 10)
-				_putchar(' ');
-			else if (result >= 100)
-				_putchar(result / 100 + '0');
-
-			if (result >= 10)
-				_putchar ((result % 100) / 10 + '0');
-			_putchar (result % 10 + '0');
+			for (j = 0; j < n + 1; j++)
+			{
+				k = i * j;
+				if (k < 10)
+				{
+					if (j != 0)
+					{
+						_putchar(' ');
+						_putchar(' ');
+					}
+					_putchar('0' + k);
+				}
+				else if (k >= 10 && k < 100)
+				{
+					_putchar(' ');
+					_putchar('0' + k / 10);
+					_putchar('0' + k % 10);
+				}
+				else if (k >= 100)
+				{
+					_putchar('0' + k / 100);
+					_putchar('0' + (k / 10) % 10);
+					_putchar('0' + k % 10);
+				}
+				if (j < n)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
+
 }
