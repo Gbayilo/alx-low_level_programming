f63442c506ca1b78ee2d26107e7a50a03af5e02e
oluwagbayilo <princegeeby1@gmail.com>
2023-08-31 10:43:50 +0100

    Adds solution to task 6

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,10 +7,11 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int is_prime_number(int n);
 
 
 
-
+/* Help Function */
 int sqrt_helper(int n, int guess);
-
+int is_prime_helper(int n, int divisor);
 #endif
