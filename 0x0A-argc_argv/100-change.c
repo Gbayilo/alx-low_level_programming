73ac97312725196c9c90376d8016b28e3555a8b9
oluwagbayilo <princegeeby1@gmail.com>
2023-09-02 00:55:01 +0100

    Fix code errors

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,7 +10,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int cents, coins;
+	int cents, coins = 0;
 
 	if (argc != 2)
 	{
@@ -23,34 +23,24 @@ int main(int argc, char *argv[])
 		printf("0\n");
 		return (0);
 	}
-	coins = 0;
 	while (cents > 0)
 	{
 		if (cents >= 25)
-		{
 			cents -= 25;
-			coins++;
-		}
+
 		else if (cents >= 10)
-		{
 			cents -= 10;
-			coins++;
-		}
+
 		else if (cents >= 5)
-		{
 			coins -= 5;
-			coins++;
-		}
+
 		else if (cents >= 2)
-		{
 			cents -= 2;
-			coins++;
-		}
+
 		else
-		{
 			cents -= 1;
-			coins++;
-		}
+		coins++;
+
 	}
 	printf("%d\n", coins);
 	return (0);
