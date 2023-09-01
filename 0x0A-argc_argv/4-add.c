a108b4c977da78c9606edbba12d583ec835eb0a1
oluwagbayilo <princegeeby1@gmail.com>
2023-09-01 23:56:39 +0100

    Adds solution to task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * is_positive_number - Check if a string is a positive number
+ * @str: The string to check
+ *
+ * Return: 1 if it is a positive number, 0 otherwise.
+ */
+int is_positive_number(char *str)
+{
+	while (*str)
+	{
+		if (!isdigit(*str))
+		{
+			return (0);
+		}
+	}
+	str++;
+	return (1);
+}
+
+/**
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments
+ * @argv: An array of strings containing the arguments
+ *
+ * Return: 0 if successful, 1 if there's an error
+ */
+int main(int argc, char *argv[])
+{
+	int sum, i;
+
+	(void)argv;
+
+	sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		if (is_positive_number(argv[i]))
+		{
+			sum += atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,5 +2,8 @@
 #define MAIN_H
 int main(int argc, char *argv[]);
 
+/*Help Functions*/
+int is_positive_number(char *str);
+
 
 #endif
