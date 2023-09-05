0ff51d69fa703a00d0e47a10057e6b0bb0cd6550
oluwagbayilo <princegeeby1@gmail.com>
2023-09-05 21:21:39 +0100

    Update solution to task 5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -12,43 +12,35 @@
  */
 char *argstostr(int ac, char **av)
 {
-	char *concatenated;
-	int i, j, totalLength = 0;
-	int newlineCount = ac - 1;
+	char *str, *a;
+	int i, j, totalLength;
 
 	if (ac == 0 || av == NULL)
-	{
 		return (NULL);
+	for (i = 0, totalLength = 0; i < ac; i++)
+	{
+		for (j = 0; *(*(av + i) + j) != '\0'; j++, totalLength++)
+			;
+		totalLength++;
 	}
+	totalLength++;
+
+	str = malloc(totalLength * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+
+	a = str;
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
 		{
-			totalLength++;
+			*str = av[i][j];
+			str++;
 		}
-	}
-	totalLength += newlineCount;
-	concatenated = (char *)malloc(sizeof(char) * (totalLength + 1));
+		*str = '\n';
+		str++;
 
-	if (concatenated == NULL)
-	{
-		return (NULL);
 	}
-	for (i = 0, j = 0; i < ac; i++)
-	{
-		while (av[i][j] != '\0')
-		{
-			*concatenated = av[i][j];
-			concatenated++;
-			j++;
-		}
-		if (i < ac - 1)
-		{
-			*concatenated = '\n';
-			concatenated++;
-		}
-		j = 0;
-	}
-	*concatenated = '\0';
-	return (concatenated - totalLength);
+
+	return (a);
 }
