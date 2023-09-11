ff9b6646a6595e5d3a69bea104a718a63aac65bc
oluwagbayilo <princegeeby1@gmail.com>
2023-09-12 00:01:22 +0100

    Add solution to task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Information about the dog
+ * @name: Pointer to a string representing the dog's name
+ * @age: The age of the dog as a floating-point number
+ * @owner: Pointer to a string representing the owner's name
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif
