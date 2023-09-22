5b7e898685ddbed30eef6761c22a5f21bdd8d928
oluwagbayilo <princegeeby1@gmail.com>
2023-09-22 21:29:29 +0100

    Update solution to task 6

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -5,18 +5,13 @@ section .text
     global main
 
 extern printf
-global _start
 
-_start:
+main:
     push rbp              ; Save the base pointer
     mov rdi, hello_message  ; Load the address of the message into rdi
     call printf            ; Call printf
     pop rbp               ; Restore the base pointer
-
-    ; Exit the program
-    mov rax, 60           ; syscall number for exit
-    xor rdi, rdi          ; exit status 0
-    syscall
+    ret                   ; Return
 
 section .bss
     ; Define any uninitialized data here if needed
