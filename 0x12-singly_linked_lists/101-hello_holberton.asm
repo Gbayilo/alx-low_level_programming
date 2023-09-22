10d13a4d11443667cc16ac69c206ab64eb4fc75a
oluwagbayilo <princegeeby1@gmail.com>
2023-09-22 21:28:08 +0100

    Update solution to task 6

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,23 @@
 section .data
-hello_message:	db "Hello, Holberton",10,0 
-fmt:		db "%s", 10. 0
+    hello_message db "Hello, Holberton",10,0  ; The message to print, with a newline character and null terminator
 
-	section .text
-	global main
-	extern printf
+section .text
+    global main
 
-main:
-	mov esi, hello_message
-	mov edi, fmt
-	mov eax, 0
-	call printf
+extern printf
+global _start
 
-	mov eax, 0
-	ret
+_start:
+    push rbp              ; Save the base pointer
+    mov rdi, hello_message  ; Load the address of the message into rdi
+    call printf            ; Call printf
+    pop rbp               ; Restore the base pointer
+
+    ; Exit the program
+    mov rax, 60           ; syscall number for exit
+    xor rdi, rdi          ; exit status 0
+    syscall
+
+section .bss
+    ; Define any uninitialized data here if needed
 
