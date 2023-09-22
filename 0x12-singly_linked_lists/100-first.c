2faa46226d7aa3d54cafa6a493eac933af976534
oluwagbayilo <princegeeby1@gmail.com>
2023-09-22 20:54:39 +0100

    Add solution to task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * startup_message - Function to print a message before main is executed.
+ */
+void startup_message(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
