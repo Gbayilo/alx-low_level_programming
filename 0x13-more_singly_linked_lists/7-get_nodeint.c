b95283eded5240e1109e0731a97cfa9921265fed
oluwagbayilo <princegeeby1@gmail.com>
2023-09-25 18:51:09 +0100

    Adhere to betty coding style

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -5,7 +5,8 @@
  * @head: Pointer to the head of the list.
  * @index: Index of the node to retrieve (starting at 0).
  *
- * Return: Pointer to the nth node, or NULL if the node does not exist*/
+ * Return: Pointer to the nth node.
+ */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int count = 0;
