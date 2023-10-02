6800077bc1e3fae6175ab57e085294de805f1a88
oluwagbayilo <princegeeby1@gmail.com>
2023-10-02 22:18:53 +0100

    Add a header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -27,5 +27,6 @@ void print_osabi(unsigned char *e_ident);
 void print_type(unsigned int e_type, unsigned char *e_ident);
 void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 void close_elf(int elf);
+void print_data(unsigned char *e_ident);
 
 #endif
