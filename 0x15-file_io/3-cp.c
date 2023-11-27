d75ed60fe49be21074582fcd45fc6d1753dde9b0
oluwagbayilo <princegeeby1@gmail.com>
2023-11-27 13:32:14 +0100

    Add solution to task 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,90 @@
+#include "main.h"
+
+/**
+* allocate_buffer - Allocates 1024 bytes for a buffer.
+* @filename: The name of the file the buffer stores for char.
+*
+* Return: A pointer to the newly created buffer.
+*/
+char *allocate_buffer(char *filename)
+{
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * 1024);
+
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO,
+		"Error: Can't write to %s\n", filename);
+		exit(99);
+	}
+	return (buffer);
+}
+
+/**
+* close_file_descriptor - Closes file descriptors.
+* @file_descriptor: The file descriptor to be closed.
+*/
+void close_file_descriptor(int file_descriptor)
+{
+	int result;
+
+	result = close(file_descriptor);
+	if (result == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_descriptor);
+		exit(100);
+	}
+}
+
+/**
+* main - Copies the contents of a file to another file.
+* @argc: The number of arguments recieved by the program.
+* @argv: An array of pointers to the arguments.
+*
+* Return: 0 on success.
+*/
+int main(int argc, char *argv[])
+{
+	int src_fd, dest_fd, read_bytes, write_bytes;
+	char *buffer;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+
+	}
+
+	buffer = allocate_buffer(argv[2]);
+	src_fd = open(argv[1], O_RDONLY);
+	read_bytes = read(src_fd, buffer, 1024);
+	dest_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	do {
+		if (src_fd == -1 || read_bytes == -1)
+		{
+			dprintf(STDERR_FILENO,
+			"Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
+			exit(98);
+		}
+
+		write_bytes = write(dest_fd, buffer, read_bytes);
+		if (dest_fd == -1 || write_bytes == -1)
+		{
+			dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", argv[2]);
+			free(buffer);
+			exit(99);
+		}
+		read_bytes = read(src_fd, buffer, 1024);
+		dest_fd = open(argv[2], O_WRONLY | O_APPEND);
+
+	} while (read_bytes > 0);
+	free(buffer);
+	close_fd(src_fd);
+	close_fd(dest_fd);
+	return (0);
+}
+
