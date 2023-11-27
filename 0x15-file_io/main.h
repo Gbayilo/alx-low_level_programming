8525a8f4acf04ec1d7d217c181f973f93d0b39af
oluwagbayilo <princegeeby1@gmail.com>
2023-11-27 13:38:26 +0100

    Add solution to task 4

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,6 +11,17 @@
 #include <stdio.h>
 #include <elf.h>
 
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
+void print_data(unsigned char *e_ident);
+
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
@@ -18,4 +29,15 @@ int append_text_to_file(const char *filename, char *text_content);
 char *allocate_buffer(char *file);
 void close_fd(int fd);
 
+/*Functions for task 4*/
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
+void print_data(unsigned char *e_ident);
 #endif
