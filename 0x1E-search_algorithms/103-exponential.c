7dfdc0921536348f3cf799d8ee6b546c63661c8e
Oluwagbayilo Odetunde <princegeeby1@gmail.com>
2024-07-16 13:58:27 +0100

    test solution

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -13,15 +13,15 @@
 int recursive_search(int *array, size_t size, int value)
 {
 	size_t half = size / 2;
-	size_t j;
+	size_t i;
 
 	if (array == NULL || size == 0)
 		return (-1);
 
 	printf("Searching in array");
 
-	for (j = 0; j < size; j++)
-		printf("%s %d", (j == 0) ? ":" : ",", array[j]);
+	for (i = 0; i < size; i++)
+		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
 
 	printf("\n");
 
@@ -37,6 +37,27 @@ int recursive_search(int *array, size_t size, int value)
 	half++;
 
 	return (recursive_search(array + half, size - half, value) + half);
+}
+
+/**
+ * binary_search - calls to binary_search to return
+ * the index of the number
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	int index;
+
+	index = recursive_search(array, size, value);
+
+	if (index >= 0 && array[index] != value)
+		return (-1);
+
+	return (index);
 }
 
 /**
@@ -79,25 +100,4 @@ int exponential_search(int *array, size_t size, int value)
 		result += index;
 
 	return (result);
-}
-
-/**
- * binary_search - calls to binary_search to return
- * the index of the number
- *
- * @array: input array
- * @size: size of the array
- * @value: value to search in
- * Return: index of the number
- */
-int binary_search(int *array, size_t size, int value)
-{
-	int index;
-
-	index = recursive_search(array, size, value);
-
-	if (index >= 0 && array[index] != value)
-		return (-1);
-
-	return (index);
 }
