af0090098fe82b6d97b01e74c29d3b6f9e0d0c95
Oluwagbayilo Odetunde <princegeeby1@gmail.com>
2025-08-04 09:15:58 +0100

    feat(0x01): print the alphabet in lowercase
    Implements task 2 of the ALX '0x01. C - Variables, if, else, while' project.
    
    The program prints the alphabet in lowercase, followed by a new line
    using only the putchar function.

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+
 /**
- * main - Entry point
- * Return: Always 0
+ * main - Entry point of the program
+ *
+ * Description: Prints the alphabet in lowercase,
+ * followed by a new line
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-char low;
-for (low = 'a'; low <= 'z'; low++)
-putchar(low);
-putchar ('\n');
-return (0);
+	int c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
 }
